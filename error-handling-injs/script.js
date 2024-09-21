baf14c747dab74b6846c348176bb66207386787c
ponnanapavan <pavanponnana1@gmail.com>
2024-09-21 22:45:54 +0530

    error-handling

---
+++ error-handling-injs/script.js
@@ -0,0 +1,31 @@
+//if the normal flow of excution will be fail then it is know an error
+//types of errors
+//1)compile-time   2)runtime
+
+//handle the error
+
+try{
+     
+     console.log(x);
+
+}catch(err){
+     console.log('i am inside catch block')
+     console.log(err.message)
+}finally{
+
+    console.log("i am finally block")
+
+}
+//finally block will run every time 
+
+//we can also throw cutom error using throw keyword
+
+try{
+
+     console.log(x);
+
+}catch(err){
+
+    throw new Error("some thing went wrong!!!!")
+
+}
