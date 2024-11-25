a24fc99386f6ce1b13c4e6650a355bc8531e9dcc
ponnanapavan <pavanponnana1@gmail.com>
2024-11-25 11:25:34 +0530

    promsie added

--- callback-promises-asunc-await-in-js/script.js
+++ callback-promises-asunc-await-in-js/script.js
@@ -1,38 +1,32 @@
 //synchronous
-//it refers means the code run in particular sequence of instructions given in the program Each instructions waits for the previous instruction to complete its excution 
-
+//it refers means the code run in particular sequence of instructions given in the program Each instructions waits for the previous instruction to complete its excution
 
 //asynchronous
 //due to synchronous programming sometimes instructions get blocked due to some previous instructions,which cause delay in uiAsynchronous code excution allows to excute next instructions without block
 
-
-
 // console.log(1);
 // console.log(2);
 
-
 // setTimeout(()=>console.log("ponnanapavan"),1000)//2 milliseconds
 // console.log(3)
-// console.log(4) 
+// console.log(4)
 // //here last two statements doesn't wait untill 2 seconds
-
 
 //----------------------------------------------------------------------------------------------------------------->
 
+//callback-->a callback is a function which is  use argument for  another function
 
-// //callback-->a callback is a function which is arguement of a another function 
+// due to callback we will async things in js
 
-// function calculator(a,b,sum){
-//       sum(a,b);
-// }
+function calculator(a, b, sum) {
+  sum(a, b);
+}
 
-// calculator(2,5,function(a,b){
+calculator(2, 5, function (a, b) {
+  console.log(a + b);
+});
 
-//     console.log(a+b);
-      
-// })
-
-//-------------------------------------------------------------------------------------------------------------------------->
+// -------------------------------------------------------------------------------------------------------------------------->
 
 //callback hell--> Nested callbacks stacked below one another forming a pyramid structure
 // let age=20;
@@ -47,11 +41,10 @@
 
 //     console.log("child")
 
-
 // }
 
 // function data(dataId,getNextData){
-   
+
 //       setTimeout(()=>{
 //         console.log(dataId)
 //         if(getNextData)
@@ -67,8 +60,7 @@
 //      })
 // });//callback hell
 
-
-//real time example 
+//real time example
 
 // function getUser(userId, callback) {
 //     setTimeout(() => {
@@ -76,21 +68,20 @@
 //       callback({ userId, name: "John Doe" });
 //     }, 5000);
 //   }
-  
+
 //   function getPosts(userId, callback) {
 //     setTimeout(() => {
 //       console.log("Fetched posts for user:", userId);
 //       callback([{ postId: 1, title: "Post 1" }, { postId: 2, title: "Post 2" }]);
 //     }, 5000);
 //   }
-  
+
 //   function getComments(postId, callback) {
 //     setTimeout(() => {
 //       console.log("Fetched comments for post:", postId);
 //       callback(["Great post!", "Thanks for sharing"]);
 //     }, 5000);
 //   }
-
 
 //   getUser(1,(user)=>{
 //          getPosts(user.userId,(posts)=>{
@@ -100,7 +91,6 @@
 //          })
 //   })
 
-
 //------------------------------------------------------------------------------------------------------------->
 
 //promise
@@ -108,26 +98,23 @@
 
 //A promise is a object that tells whether asynchronous operation is sucess or not
 
-
 // let promise=new Promise((resolve,reject)=>{
 
 //     console.log("i am a promsie")
 //     reject("some error")
 
-       
 // })
-
 
 // function data(dataId,getNextData){
 //      return new Promise((resolve,reject)=>{
 //         setTimeout(()=>{
-          
+
 //             // resolve(dataId)
 //             reject("vhfvfv")
 //               if(getNextData)
 //                 getNextData();
 //         },5000)
-           
+
 //      })
 // }
 
@@ -140,8 +127,7 @@
 //then will be excute when a promise is fullfilled
 //catch will be excute when a promise will be rejected
 
-
-//promise chain 
+//promise chain
 
 // function asynFunc(){
 
@@ -149,10 +135,10 @@
 //           setTimeout(()=>{
 //              console.log("some data1")
 //              resolve("success")
-                
+
 //           },4000)
 //     })
-     
+
 // }
 
 // function asynFunc2(){
@@ -161,12 +147,11 @@
 //           setTimeout(()=>{
 //              console.log("some data2")
 //              resolve("success")
-                
+
 //           },4000)
 //     })
-     
-// }
 
+// }
 
 // asynFunc().then((res)=>{
 //     console.log(res)
@@ -179,9 +164,7 @@
 //     })
 // })
 
-
 //-------------------------------------------------------------------------------------------------------------------->
-
 
 //example for promise chaining
 // function getUser(userId) {
@@ -192,7 +175,7 @@
 //       }, 1000);
 //     });
 //   }
-  
+
 //   function getPosts(userId) {
 //     return new Promise((resolve) => {
 //       setTimeout(() => {
@@ -201,7 +184,7 @@
 //       }, 1000);
 //     });
 //   }
-  
+
 //   function getComments(postId) {
 //     return new Promise((resolve) => {
 //       setTimeout(() => {
@@ -210,7 +193,7 @@
 //       }, 1000);
 //     });
 //   }
-  
+
 //   // Chaining Promises to avoid callback hell
 //   getUser(1)
 //     .then((user) => getPosts(user.userId))
@@ -219,8 +202,6 @@
 //       console.log("Comments on first post:", comments);
 //     })
 //     .catch((error) => console.error(error));
-  
-
 
 //example -2  returning the promise to another then method
 
@@ -235,7 +216,7 @@
 //       }, 4000); // Set the delay to 4 seconds
 //     });
 //   }
-  
+
 //   Pavan(15)
 //     .then((res) => {
 //       console.log(res); // First "success" output
@@ -247,28 +228,25 @@
 //     .catch((err) => {
 //       console.error(err); // If any Promise fails, handle the error here
 //     });
-  
+
 //------------------------------------------------------------------------------------------------------------>
-//async and await 
-//await pauses the execution of its surrounding async function until the promise is 
+//async and await
+//await pauses the execution of its surrounding async function until the promise is
 
-
-async function getWeather(){
-       const data=  await api();
-       console.log(data)
-         console.log("ponana")
+async function getWeather() {
+  const data = await api();
+  console.log(data);
+  console.log("ponana");
 }
 
- function api(){
-      return new Promise((resolve,reject)=>{
-          console.log("wether data");
-           setTimeout(()=>{
-            resolve(200);
-           },4000)
-      })
-          
+function api() {
+  return new Promise((resolve, reject) => {
+    console.log("wether data");
+    setTimeout(() => {
+      resolve(200);
+    }, 4000);
+  });
 }
 
-getWeather()
-console.log("nvjvfvhvfjvn")
-
+getWeather();
+console.log("nvjvfvhvfjvn");
---
+++ promises/script.js
@@ -0,0 +1,46 @@
+const cart = ["ponnnaa", "pants", "kurta"];
+
+creatOrder(cart)
+  .then((orderId) => {
+    return orderId;
+  })
+  .then((orderId) => {
+    return processToPayment(orderId);
+  })
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err.message));
+
+function creatOrder(cart) {
+  const pr = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!validateCart(cart)) {
+        const err = new Error("Cart is not valid");
+        reject(err);
+      }
+      const orderId = "123455";
+      if (orderId) resolve(orderId);
+    }, 5000);
+  });
+
+  return pr;
+}
+
+function processToPayment(orderId) {
+  const pr = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (orderId) resolve("payment succes");
+      reject(new Error("payment failed"));
+    }, 7000);
+  });
+  return pr;
+}
+
+function validateCart(cart) {
+  return true;
+}
+
+//resolve and reject are methods which given by javascript
+//resolve means promise successfull and reject means promise failed
+//intially promise is pending after promise fullfilled then then method will run but if promise is failed the catch method will excute
+
+//then method will return a promise.If we want to excute a series of asynchronou promises then we have  to use promise chaining.
