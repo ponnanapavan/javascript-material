57dd2307d0e157a1e15db621ea0783a919a30669
ponnana pavan <pavanponnana1@gmailcom>
2024-12-13 11:24:33 +0530

    this

--- this-in-javascript/index.js
+++ this-in-javascript/index.js
@@ -5,7 +5,7 @@
 // //in the broswer global object is window(represent to window/tab where our webpage runs) but in Nodejs global object is global
 
 // function x() {
-//   console.log(this);
+//   console.log(this);  //here this pointed to global window object
 // }
 
 // //value of this keyword inside the function  is depend  on strict mode or non strict mode .In strict mode value is undefined and in the non-strict mode value is global object either window or global
@@ -65,10 +65,80 @@ const obj2 = {
   a: 20,
   x: function () {
     const y = () => {
-      console.log(this);
+      console.log(this); // this is pointed to obj2   because for arrow function does't have its own this so it it will take the this from it's lexical scope
     };
     y();
   },
 };
 
 obj2.x();
+
+class user {
+  constructor(n) {
+    this.name = n;
+  }
+
+  getname() {
+    console.log(this.name);
+  }
+}
+
+const User = new user("ponnana");
+
+User.getname();
+
+//interview question
+
+function makeUser() {
+  return {
+    name: "ponnana",
+    ref() {
+      return this;
+    },
+  };
+}
+
+let user1 = makeUser();
+console.log(user1.ref().name);
+
+// const user2 = {
+//   name: "ponnana pavan",
+//   logMessage() {
+//     console.log(this.name);
+//   },
+// };
+
+// setTimeout(user2.logMessage, 1000);
+// //here passing the function without object so when the function will run after 1 second thus is no longer in user2 it will point to window object
+
+// const user2 = {
+//   name: "ponnana pavan",
+//   logMessage() {
+//     console.log(this.name);
+//   },
+// };
+
+// setTimeout(function () {
+//   user2.logMessage();
+// }, 1000);
+
+// //create a object constructor
+
+// let calculator = {
+//   read() {
+//     console.log(this);
+//     this.a = prompt("a + ", 0);
+//     this.b = prompt("b + ", 0);
+//   },
+
+//   sum() {
+//     return this.a + this.b;
+//   },
+//   mul() {
+//     return this.a * this.b;
+//   },
+// };
+
+// calculator.read();
+// console.log(calculator.sum());
+// console.log(calculator.mul);
